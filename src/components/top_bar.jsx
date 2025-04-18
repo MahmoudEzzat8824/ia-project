6d2f6dcf4b691bcc75f6639749d243dd726bbbf3
Mahmoud Kaarem <fankyelsoayar@gmail.com>
2025-04-18 18:20:24 +0200

    topbarEdited

--- src/components/top_bar.jsx
+++ src/components/top_bar.jsx
@@ -2,13 +2,15 @@ import website_logo from '../assets/website_logo.png';
 
 function TopBar() {
     return (
-        <>
-            <div className="top_bar">
-                <a className="website_name" href="/">BookSwap</a>
-                <img src={website_logo} alt="BookSwap logo" className='web_logo'/>
+        <div className="top_bar">
+            <a className="website_name" href="/">BookSwap</a>
+            <a className="TopBarProfilePageLink" href="/ProfilePage">Profile</a>
+            <div className="search-container">
+                <input type="text" className="search-input" placeholder="Search..." />
+                <span className="search-icon">🔍</span>
             </div>
-        </>
-        
+            <img src={website_logo} alt="BookSwap logo" className="web_logo" />
+        </div>
     );
 }
 
